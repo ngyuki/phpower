405c81d0b06fbd3a6cab76135b3cf7cc873369bf
Toshiyuki Goto <ngyuki.jp@gmail.com>
2020-02-05 23:11:35 +0900

    Check if FileLoader is already loaded

--- src/autoload.php
+++ src/autoload.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 namespace ngyuki\Phpower;
 
 use Microsoft\PhpParser\Node;
+use PHPUnit\Util\FileLoader;
 use PHPUnit\Util\Filesystem;
 use ReflectionClass;
 
 StreamFilter::register();
 
-if (class_exists(Filesystem::class, true)) {
+if (!class_exists(FileLoader::class, false) && class_exists(Filesystem::class, true)) {
     // @phan-suppress-next-line PhanPossiblyFalseTypeArgumentInternal
     $dir = dirname((new ReflectionClass(Filesystem::class))->getFileName());
-
-    StreamFilter::load($dir . DIRECTORY_SEPARATOR . '/FileLoader.php', function (string $source) {
+    $file = $dir . DIRECTORY_SEPARATOR . '/FileLoader.php';
+    StreamFilter::load($file, function (string $source) {
         return (new NodeTraverser(function (Node $node, callable $next) {
             if ($node instanceof Node\Expression\ScriptInclusionExpression) {
                 return $node->requireOrIncludeKeyword->getLeadingCommentsAndWhitespaceText($node->getFileContents())
